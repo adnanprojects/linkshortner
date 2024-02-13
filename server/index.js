4fab9aa104967c0c82975044c107917a381ef406
adnanprojects <adnanchaudhary99@gmail.com>
2024-02-13 22:23:46 +0530

    Update backend

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(morgan('tiny'));
 app.use(helmet());
 app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/', (request, response) => {
     response.json({
